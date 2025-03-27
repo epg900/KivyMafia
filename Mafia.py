2d0aa765d4f35befb4488bcda156b326db175d89
Ersci <47410365+epg900@users.noreply.github.com>
2025-03-28 02:03:14 +0330

    Update Mafia.py

--- Mafia.py
+++ Mafia.py
@@ -1,7 +1,5 @@
 #Mafia.py
 #-*- coding: utf-8 -*-
-#super(Scr1, self).__init__()
-#<img src="dist/im.jpg" alt='m' />
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -9,40 +7,97 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.image import Image
+from kivy.uix.filechooser import FileChooserListView
 import random
-import arabic_reshaper
-import bidi.algorithm
+import os, subprocess
 import time
+import glob
+
+
 
 class Scr1(GridLayout):
    def __init__(self):
      super().__init__()
-     self.cols=2
-     file = open("mafia.txt", "r",encoding='utf-8')
-     self.lst= file.readlines()
-     file.close()
-     random.shuffle(self.lst)
-     a=len(self.lst)
-     for i in range(0,a):
-     	 self.btn= Button(text=str(i+1),background_color =(0.2,0.9, 1, 1),font_name='BTitr.ttf',font_size=50)
-     	 self.btn.bind(on_press=self.callback)
-     	 self.add_widget(self.btn)
-  
-   def callback(self,instance):
+     self.game_path=""
+     self.parent_game=""
+     self.cols=1
+     self.rows=2
+     self.btn= Button(text="Select  Folder",background_color =(0.7,0.7,0.7, 1),font_size=40,halign="center", size_hint=(1,0.2))
+     self.btn.bind(on_press=self.callback)
+     self.fc = FileChooserListView(dirselect = True,filters=[self.is_dir])
+     self.fc.bind(selection=self.getdir)
+     self.add_widget(self.fc)
+     self.add_widget(self.btn)
      
-     try:
-      txt=arabic_reshaper.reshape(self.lst[int(instance.text)-1])
-      txt2=bidi.algorithm.get_display(txt)
-      #popup = Popup(title='Your Role',content=Label(text=txt2,font_name='BTitr.ttf',font_size=50,base_direction='rtl'))
-      self.popup=Popup(title='',content=Button(text=txt2,font_name='BTitr.ttf',font_size=50,base_direction='rtl' , on_press = lambda *args: self.popup.dismiss()))
-      self.popup.open()
-      #time.sleep(8)
-      #popup.dismiss()
-      instance.disabled=True
+   
+   def callback(self,instance):
+     	try:
+     		self.lst =glob.glob(f"{self.game_path}/*.jpg")
+     		self.lst.sort()
+     		self.lst=[l.split("/")[-1] for l in self.lst]
      		
-     except:
-     	pass
-
+     		e=[1,2,4]
+     		
+     		file=open("role.txt","r",encoding="utf-8")
+     		self.lst2=file.readlines()
+     		file.close()
+     		self.lst2=[l.split("\n")[0] for l in self.lst2]
+     		
+     		random.shuffle(self.lst)
+     		self.lst3=[]
+     		for l in self.lst2:
+     			if l in self.lst:
+     				self.lst3.append(l)
+     		random.shuffle(self.lst3)
+     		
+     		for i,v in enumerate(e):
+     			if e[i]:
+     				ix1=self.lst.index(self.lst3[i])
+     				iv1=self.lst[e[i]-1]
+     				self.lst[e[i]-1]=self.lst3[i]
+     				self.lst[ix1]=iv1
+     		
+     		self.length=len(self.lst)
+     		self.j = 0
+     		self.btn2=Button(text="Select Role",background_normal=' ',background_color=(0.5,0.5,0.5,1),font_size=50)
+     		self.btn2.bind(on_press=self.call2)
+     		self.popup=Popup(title='',content=self.btn2)
+     		self.popup.open()
+     	except:
+     		pass
+   
+   def getdir(self,path, fn):
+         self.game_path=fn[0]
+         self.parent_game=os.path.dirname(fn[0
+        ])
+         txt=fn[0].split("/")[-1]
+         self.btn.text=f" [ {txt} ] \n\nStart Game"
+         
+   def is_dir(self, directory, filename):
+        return os.path.isdir(os.path.join(directory, filename))
+   
+   def call2(self,instance):
+     	if instance.text=="End":
+     		self.popup.dismiss()
+     	if self.j<self.length*2:
+     		self.j+=1
+     	else:
+     		with open(f'{self.parent_game}/all_role.txt','w') as f:
+     			for i,l in enumerate(self.lst):
+     				f.write(f'{i+1}  >>   {l}\n')
+     		
+     	if self.j%2==1:
+     		instance.text=""
+     		instance.background_normal=self.game_path + "/" + self.lst[int(self.j/2)]
+     	else:
+     		if self.j<self.length*2-1:
+     			 instance.text="Select Role"
+     			 instance.background_normal=' '
+     		else:
+     			instance.text="End"
+     			instance.background_normal=' '
+     			
 class MyApp(App):
    def build(self):
    	return Scr1()
